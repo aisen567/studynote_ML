218240041c208ce1127809baf3abc647aebd1c23
azusakou <52435819+azusakou@users.noreply.github.com>
2021-09-02 20:12:31 +0900

    Update create_scheduler.py

--- Deep-learn/CV_segmentation/lesson/lesson-19/create_scheduler.py
+++ Deep-learn/CV_segmentation/lesson/lesson-19/create_scheduler.py
@@ -5,6 +5,49 @@
 # @date       : 2019-09-07 10:08:00
 # @brief      : 人民币分类模型训练
 """
+
+
+# ===================================================================
+import torch
+import torch.nn as nn
+from torch.optim.lr_scheduler import LambdaLR
+import itertools
+
+
+initial_lr = 0.1
+
+class model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3)
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3)
+
+    def forward(self, x):
+        pass
+
+net_1 = model()
+
+optimizer_1 = torch.optim.Adam(net_1.parameters(), lr = initial_lr)
+scheduler_1 = LambdaLR(optimizer_1, lr_lambda=lambda epoch: 1/(epoch+1))
+
+print("初始化的学习率：", optimizer_1.defaults['lr'])
+
+for epoch in range(1, 11):
+    # train
+
+    optimizer_1.zero_grad()
+    optimizer_1.step() # update weights
+    print("第%d个epoch的学习率：%f" % (epoch, optimizer_1.param_groups[0]['lr']))
+    scheduler_1.step() # 更新学习率
+# ===================================================================
+
+
+
+
+
+
+
+
 import os
 import random
 import numpy as np
